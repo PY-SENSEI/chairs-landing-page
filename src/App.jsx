1bd57b5d28a9cf8d5ebea85379a80baf157c47e3
PY-SENSEI <gauravjena48@gmail.com>
2026-01-02 22:43:41 +0530

    first part

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="min-h-screen overflow-clip">
+      {/* Header */}
+      <Header />
+      
+      <main>{/* Sections */}</main>
+      
+      {/* footer */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Menu, Phone } from "lucide-react";
+
+import NavBtn from "./NavBtn";
+
+const Header = () => {
+  return (
+    <header
+      className="fixed border-b border-b-zinc-300 bg-white/50 backdrop-blur-lg min-w-full lg:px-7 px-4
+    sm:px-8 mx-w-7xl overflow-x-auto"
+    >
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <div className="text-3xl impact relative uppercase tracking-tight text-zinc-900">
+          Furniture<sup className="absolute font-thin text-xs">TM</sup>
+        </div>
+
+        {/* Desktop nav */}
+        <nav className="hidden lg:flex items-center space-x-8">
+          <NavBtn>Home</NavBtn>
+          <NavBtn>About</NavBtn>
+          <NavBtn>Works</NavBtn>
+          <NavBtn>Gallery</NavBtn>
+          <NavBtn>Service</NavBtn>
+
+          {/* Contact Button  */}
+          <button
+            className="flex items-center space-x-2 bg-zinc-900 text-white text-sm py-4 px-6 rounded-full cursor-pointer
+             transition-all duration-150 ease-in ml-4 hover:bg-transparent border border-zinc-900 hover:text-zinc-900 "
+          >
+            <Phone size={16} />
+               <span>Contact Us</span>
+          </button>
+        </nav>
+        
+        {/* Mobile Menu icon */}
+        <div className="lg:hidden">     
+            <Menu className="size-10 text-zinc-900" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
